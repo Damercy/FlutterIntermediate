e1b65f5810bba26a9b814ddc6f10ec5120fbec92
Damercy <adhikaridayamoy@gmail.com>
2021-01-25 02:21:02 +0530

    Adding tests

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,15 +1,24 @@
+import 'dart:convert';
+import 'package:UdemyFlutter/src/models/item_model.dart';
 import 'package:http/http.dart' show Client;
+
+// prefixing with _ makes variable private; without _ it's public and automatically exported
+final _baseUrl = "https://hacker-news.firebaseio.com/v0";
 
 class NewsApiProvider{
 
   Client client = Client();
 
   fetchTopIds() async{
-      var response = await client.get("https://hacker-news.firebaseio.com/v0/topstories.json");
+      final response = await client.get("$_baseUrl/topstories.json");
+      final ids = jsonDecode(response.body);
+      return ids;
   }
 
-  fetchItem(){
-
+  fetchItem(int id) async {
+    final response = await client.get("$_baseUrl/item/$id.json");
+    final item = jsonDecode(response.body);
+    return ItemModel.fromJson(item);
   }
 
 }
