47f1444755851c00e569f5e74eb9be80fcd7ba77
Damercy <adhikaridayamoy@gmail.com>
2021-01-24 23:12:44 +0530

    Added setstate for image_list

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,11 @@
 // import required packages
+import 'dart:convert';
+import 'package:UdemyFlutter/src/models/image_model.dart';
 import 'package:flutter/material.dart';
+import 'widgets/widget_getter_functions/appbar.dart';
+
+// Means just import the get function out of everything; Performance boost
+import 'package:http/http.dart' show get;
 
 // define a class as stateful or stateless
 // Must define build() with context paramteter
@@ -10,22 +16,33 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  int imageIdCounter = 1;
+  List<ImageModel> imageList;
+
+  void fetchImage() async {
+    imageIdCounter++; // Not in setState() as we don't care about displaying updating value
+    // State is however maintained
+
+    var response = await get(
+        "https://jsonplaceholder.typicode.com/photos/$imageIdCounter");
+
+    var imageModel = ImageModel.fromJson(jsonDecode(response.body));
+
+    setState(() {
+      imageList.add(imageModel);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            'Hello!',
-            style: TextStyle(
-                color: Colors.black54,
-                fontWeight: FontWeight.bold,
-                fontSize: 30.0),
-          ),
-          backgroundColor: Colors.white,
-          elevation: 0,
-        ),
+        appBar: headerAppBar("Image fetcher"),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add_a_photo),
+          splashColor: Colors.blueAccent.shade700,
+          onPressed: fetchImage,
+        ), // NOTE: If a function calls another function, we can just pass the function reference as done here.
       ),
     );
   }
