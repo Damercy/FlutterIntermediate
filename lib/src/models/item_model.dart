7ef91ad40494e3a814c3cfee7a86a5b895b6dc1e
Damercy <adhikaridayamoy@gmail.com>
2021-01-25 03:23:11 +0530

    added fromDb() in model

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ItemModel {
   final int id;
   final bool deleted;
@@ -27,4 +29,19 @@ class ItemModel {
         score = jsonData['score'],
         title = jsonData['title'],
         descendants = jsonData['descendants'];
+
+  ItemModel.fromDb(Map<String, dynamic> dbData)
+      : id = dbData['id'],
+        deleted = dbData['deleted']==1,
+        type = dbData['type'],
+        by = dbData['by'],
+        time = dbData['time'],
+        text = dbData['text'],
+        dead = dbData['dead']==1,
+        parent = dbData['parent'],
+        kids = jsonDecode(dbData['kids']),
+        url = dbData['url'],
+        score = dbData['score'],
+        title = dbData['title'],
+        descendants = dbData['descendants'];
 }
--- lib/src/resources/news_db-provider.dart
+++ lib/src/resources/news_db-provider.dart
@@ -4,3 +4,52 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'dart:async';
 import '../models/item_model.dart';
+
+class NewsDbProvider{
+  Database db;
+
+  init() async{
+
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+
+    final path = join(documentDirectory.path,"items.db");
+
+    db = await openDatabase(
+    path,
+    version: 1,
+    onCreate: (Database newDb,int version){
+
+      // New database created; setup tables;
+      newDb.execute("""
+      CREATE TABLE items(
+        id INTEGER PRIMARY KEY,
+        type TEXT,
+        by TEXT,
+        time INTEGER,
+        text TEXT,
+        parent INTEGER,
+        kids BLOB,
+        dead INTEGER,
+        deleted INTEGER,
+        url TEXT,
+        title TEXT,
+        descendants INTEGER
+        )
+      """);
+    });
+  }
+
+  fetchItem(int id)async{
+    final result =  await db.query(
+      "items",
+      columns: null,
+      where: "$id=?",
+      whereArgs: [id]
+      );
+
+      if(result.isNotEmpty)
+        return ItemModel.fromDb(result.first);
+
+      return null;
+  }
+}
