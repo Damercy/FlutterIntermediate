482b4c07873df8d934214ec2acae2bfa21201ab2
Damercy <adhikaridayamoy@gmail.com>
2021-01-24 22:09:08 +0530

    Added image model class

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 
 // define a class as stateful or stateless
 // Must define build() with context paramteter
-class App extends StatelessWidget {
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
