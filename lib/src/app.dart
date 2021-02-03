967fb57bdbc604e1ddc83f5002665ba91d5be7d0
Damercy <adhikaridayamoy@gmail.com>
2021-02-04 01:02:15 +0530

    Added news_list screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:UdemyFlutter/src/screens/news_list.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatefulWidget {
@@ -8,14 +9,6 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hacker News"),
-          centerTitle: true,
-        ),
-        body: Container(),
-      ),
-    );
+    return MaterialApp(title: "Hacker news", home: NewsList());
   }
 }
